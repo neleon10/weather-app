24a6bac5b1f42bb47c515c4f3afc4448a659f78b
neleon10 <cvn45@hotmail.com>
2022-06-02 20:25:20 -0300

    cleaning input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React from "react";
 import styles from "../styles/Search.module.css";
 
-export default function SearchBar({onSearch}) {
- 
-  const [nombreCiudad,setnombreCiudad] = React.useState('');
-  
-  function onChange(e){
+export default function SearchBar({ onSearch }) {
+  const [nombreCiudad, setnombreCiudad] = React.useState("");
+
+  function onChange(e) {
     setnombreCiudad(e.target.value);
   }
 
-  return(
-    
-    <form 
-      className={styles.searchContainer} 
-      onSubmit={(e) => {
-        e.preventDefault();
-        onSearch(nombreCiudad);
-        setnombreCiudad("");  
-    }}>
+  function submit(e) {
+    e.preventDefault();
+    onSearch(nombreCiudad);
+    setnombreCiudad("");
+  }
 
-      <input  placeholder='Ingrese una ciudad...' type="text" id='inputSearch' onChange={onChange} />
-       <input type="submit"  value= 'agregar'className={styles.searchButton}/>
+  return (
+    <form className={styles.searchContainer} onSubmit={submit}>
+      <input
+        placeholder="Ingrese una ciudad..."
+        type="text"
+        id="inputSearch"
+        onChange={onChange}
+        value={nombreCiudad}
+      />
+      <input type="submit" value="agregar" className={styles.searchButton} />
     </form>
-    
-  ) 
-  };
+  );
+}
